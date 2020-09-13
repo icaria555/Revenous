8fb9b8919d158d5a665e442330d63658b3997dc4
icaria555 <icaria555@hotmail.com>
2020-09-13 23:56:53 +0700

    first commit

---
+++ src/App/App_test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import {App} from './App';
+
+let rootContainer;
+
+beforeEach(() => {
+  rootContainer = document.createElement("div");
+  document.body.appendChild(rootContainer);
+});
+
+afterEach(() => {
+  document.body.removeChild(rootContainer);
+  rootContainer = null;
+});
+
+describe("App Component Testing", () => {
+  it("Renders Hello World Title", () => {
+    //
+  });
+});
