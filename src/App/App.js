6b198fed998581df0c21660e128876d31015d149
icaria555 <icaria555@hotmail.com>
2020-09-16 00:35:58 +0700

    update code to support mocha

--- src/App/App.js
+++ src/App/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1>Hello World</h1>
         </a>
       </header>
     </div>
--- src/App/App_test.js
+++ src/App/App_test.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import {App} from './App.js';
-import { act } from "react-dom/test-utils";
+import App from './App.js';
+import act from "react-dom/test-utils";
+import { JSDOM } from 'jsdom';
+
+const assert = require('assert')
+const { window } = new JSDOM('<!doctype html><html><body></body></html>');
 
 let jsdom = require('mocha-jsdom');
-globalThis.document = jsdom({
-  url: "http://localhost"
-});
+globalThis.document = window.document;
 let rootContainer;
+
+globalThis.window = window;
+
 beforeEach(() => {
   rootContainer = document.createElement("div");
   document.body.appendChild(rootContainer);
@@ -22,6 +27,6 @@ describe("App Component Testing", () => {
   it("Renders Hello World Title", () => {
     ReactDOM.render(<App />, rootContainer);
     const h1 = rootContainer.querySelector("h1");
-    expect(h1.textContent).to.equal("Hello World");
+    assert.strictEqual(h1.textContent, "Hello World");
   });
 });
