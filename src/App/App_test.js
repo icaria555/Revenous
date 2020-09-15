55f92b5c4f3d5f5f1a2b0a44cade25afca2d8059
icaria555 <icaria555@hotmail.com>
2020-09-16 00:05:58 +0700

    fix mocha to work with react

--- src/App/App_test.js
+++ src/App/App_test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import {App} from './App.js';
+import { act } from "react-dom/test-utils";
 
+let jsdom = require('mocha-jsdom');
+globalThis.document = jsdom({
+  url: "http://localhost"
+});
 let rootContainer;
-
 beforeEach(() => {
   rootContainer = document.createElement("div");
   document.body.appendChild(rootContainer);
@@ -16,6 +20,8 @@ afterEach(() => {
 
 describe("App Component Testing", () => {
   it("Renders Hello World Title", () => {
-    //
+    ReactDOM.render(<App />, rootContainer);
+    const h1 = rootContainer.querySelector("h1");
+    expect(h1.textContent).to.equal("Hello World");
   });
 });
