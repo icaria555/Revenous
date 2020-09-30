449e49ba835d38c911dd5939ef937c2e4fd03ca4
icaria555 <icaria555@hotmail.com>
2020-09-30 23:03:00 +0700

    Updates

---
+++ precache-manifest.69042012ce05fc555baf4b0e8459d25c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfb224b43e585b9efbc26a9d8fb4cc0",
+    "url": "//icaria555.github.io/Revenous/index.html"
+  },
+  {
+    "revision": "f2895733d6827964c8d1",
+    "url": "//icaria555.github.io/Revenous/static/css/main.81f1dbee.chunk.css"
+  },
+  {
+    "revision": "390091efbb69fa8d5602",
+    "url": "//icaria555.github.io/Revenous/static/js/2.31e86184.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//icaria555.github.io/Revenous/static/js/2.31e86184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2895733d6827964c8d1",
+    "url": "//icaria555.github.io/Revenous/static/js/main.19b47952.chunk.js"
+  },
+  {
+    "revision": "301961bdb9221d86b0af",
+    "url": "//icaria555.github.io/Revenous/static/js/runtime-main.a043cf3c.js"
+  },
+  {
+    "revision": "d9fdc6c6249bb40022b76ea3c0b0d358",
+    "url": "//icaria555.github.io/Revenous/static/media/background_search_desktop.d9fdc6c6.jpg"
+  }
+]);
